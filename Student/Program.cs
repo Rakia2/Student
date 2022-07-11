c7d8fef461bd76c5a3355d6727e808168360addc
Vostro 14 <Vostro 14@DESKTOP-9U24DL8>
2022-07-11 11:13:07 +0200

    Ajoutez des fichiers projet.

---
+++ Student/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace StudentUniversity
+{
+
+ public class Person
+    {
+        string Name;
+        int Id;
+        int GPA;
+        string Field;
+
+      
+  public Person(string name,int id,int gpa,string field)
+        {
+
+            Name = name;
+            Id = id;
+            GPA = gpa;
+            Field = field;
+
+        }
+       /* public Person()
+        {
+            Name = "";
+            Id = 0;
+            GPA = 0;
+            Field = "";
+        }*/
+
+        public   void ShowGPA()
+
+        {
+
+
+            Console.WriteLine(GPA);
+
+        }
+        public static void Display( int ID, string Name,string Field)
+        {
+            Console.WriteLine(ID + "the student  is:" + Name + "" + Field);
+        }
+        
+
+            public static void Main(string[] args)
+            {
+                Person Person1 = new Person("Rakia",1,14,"Mnagement");
+                Person Person2 = new Person("Salma", 1, 14, "Marketing");
+            Console.WriteLine("Program start executing:");
+
+               Console.WriteLine("name is " +   Person1.Name);
+                Console.WriteLine( "Id is " + Person1.Id);
+               Person1.ShowGPA();
+                Console.WriteLine("study in " +  Person1.Field );
+
+
+
+
+        }
+         
+       
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
